f0df268243491058f81d9b5f93ad47a0fb84a2df
Salil Ponde <salil.ponde@gmail.com>
2023-12-22 23:09:08 +0530

    Updated site title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dokemon - Docker Container GUI",
+  title: "Dokemon - Docker Container Management GUI",
   description: "Friendly Docker Container management GUI",
 };
 
